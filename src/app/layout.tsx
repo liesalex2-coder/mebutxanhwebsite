ec9891157a809c826a27b875024ffda17f5bb0b6
liesalex2-coder <liesalex2@gmail.com>
2025-12-26 19:39:37 +0100

    Add favicon and manifest links to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,6 +74,11 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body>
         {/* Skip link pour accessibilité clavier - géré par CSS */}
